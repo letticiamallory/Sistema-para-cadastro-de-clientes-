e4bd8efbdf7368ef9b670cfbbaf46cf22f001a5e
Letticia <letticiamallory@gmail.com>
2026-02-16 19:44:49 -0300

    Adicionar arquivos de projeto.

---
+++ Cadastro de clientes visual studio/Form1.Designer.cs
@@ -0,0 +1,442 @@
+﻿namespace Cadastro_de_clientes_visual_studio
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            label16 = new Label();
+            textBox11 = new TextBox();
+            textBox12 = new TextBox();
+            textBox13 = new TextBox();
+            textBox14 = new TextBox();
+            textBox15 = new TextBox();
+            textBox16 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 11.25F);
+            label1.Location = new Point(151, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(66, 19);
+            label1.TabIndex = 0;
+            label1.Text = "Código";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(151, 53);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F);
+            label2.Location = new Point(274, 31);
+            label2.Name = "label2";
+            label2.Size = new Size(122, 20);
+            label2.TabIndex = 0;
+            label2.Text = "Nome do Cliente";
+            label2.Click += label2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(274, 53);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(386, 23);
+            textBox2.TabIndex = 1;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F);
+            label3.Location = new Point(151, 87);
+            label3.Name = "label3";
+            label3.Size = new Size(110, 20);
+            label3.TabIndex = 0;
+            label3.Text = "Documentação";
+            label3.Click += label1_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11.25F);
+            label4.Location = new Point(309, 87);
+            label4.Name = "label4";
+            label4.Size = new Size(57, 20);
+            label4.TabIndex = 0;
+            label4.Text = "Gênero";
+            label4.Click += label1_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 11.25F);
+            label5.Location = new Point(151, 147);
+            label5.Name = "label5";
+            label5.Size = new Size(28, 20);
+            label5.TabIndex = 0;
+            label5.Text = "RG";
+            label5.Click += label1_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 11.25F);
+            label6.Location = new Point(307, 147);
+            label6.Name = "label6";
+            label6.Size = new Size(86, 20);
+            label6.TabIndex = 0;
+            label6.Text = "Estado Civil";
+            label6.Click += label1_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 11.25F);
+            label7.Location = new Point(498, 147);
+            label7.Name = "label7";
+            label7.Size = new Size(145, 20);
+            label7.TabIndex = 0;
+            label7.Text = "Data de Nascimento";
+            label7.Click += label1_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 11.25F);
+            label8.Location = new Point(34, 213);
+            label8.Name = "label8";
+            label8.Size = new Size(34, 20);
+            label8.TabIndex = 0;
+            label8.Text = "CEP";
+            label8.Click += label1_Click;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 11.25F);
+            label9.Location = new Point(151, 213);
+            label9.Name = "label9";
+            label9.Size = new Size(71, 20);
+            label9.TabIndex = 0;
+            label9.Text = "Endereço";
+            label9.Click += label1_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 11.25F);
+            label10.Location = new Point(560, 213);
+            label10.Name = "label10";
+            label10.Size = new Size(26, 20);
+            label10.TabIndex = 0;
+            label10.Text = "№";
+            label10.Click += label1_Click;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 11.25F);
+            label11.Location = new Point(34, 402);
+            label11.Name = "label11";
+            label11.Size = new Size(93, 20);
+            label11.TabIndex = 0;
+            label11.Text = "Observações";
+            label11.Click += label1_Click;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 11.25F);
+            label12.Location = new Point(323, 278);
+            label12.Name = "label12";
+            label12.Size = new Size(56, 20);
+            label12.TabIndex = 0;
+            label12.Text = "Cidade";
+            label12.Click += label1_Click;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Segoe UI", 11.25F);
+            label13.Location = new Point(34, 338);
+            label13.Name = "label13";
+            label13.Size = new Size(55, 20);
+            label13.TabIndex = 0;
+            label13.Text = "Celular";
+            label13.Click += label1_Click;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Segoe UI", 11.25F);
+            label14.Location = new Point(218, 338);
+            label14.Name = "label14";
+            label14.Size = new Size(52, 20);
+            label14.TabIndex = 0;
+            label14.Text = "E-mail";
+            label14.Click += label1_Click;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Segoe UI", 11.25F);
+            label15.Location = new Point(34, 554);
+            label15.Name = "label15";
+            label15.Size = new Size(132, 20);
+            label15.TabIndex = 0;
+            label15.Text = "Situação Cadastral";
+            label15.Click += label1_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(151, 110);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(139, 23);
+            textBox3.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(309, 110);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(139, 23);
+            textBox4.TabIndex = 1;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(151, 170);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(139, 23);
+            textBox5.TabIndex = 1;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(309, 170);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(162, 23);
+            textBox6.TabIndex = 1;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(498, 170);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(162, 23);
+            textBox7.TabIndex = 1;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(34, 236);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 23);
+            textBox8.TabIndex = 1;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(151, 236);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(395, 23);
+            textBox9.TabIndex = 1;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(560, 236);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(100, 23);
+            textBox10.TabIndex = 1;
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Font = new Font("Segoe UI", 11.25F);
+            label16.Location = new Point(34, 278);
+            label16.Name = "label16";
+            label16.Size = new Size(49, 20);
+            label16.TabIndex = 0;
+            label16.Text = "Bairro";
+            label16.Click += label1_Click;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(34, 301);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(283, 23);
+            textBox11.TabIndex = 1;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(323, 301);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(337, 23);
+            textBox12.TabIndex = 1;
+            // 
+            // textBox13
+            // 
+            textBox13.Location = new Point(34, 361);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(165, 23);
+            textBox13.TabIndex = 1;
+            // 
+            // textBox14
+            // 
+            textBox14.Location = new Point(218, 361);
+            textBox14.Name = "textBox14";
+            textBox14.Size = new Size(442, 23);
+            textBox14.TabIndex = 1;
+            // 
+            // textBox15
+            // 
+            textBox15.Location = new Point(34, 425);
+            textBox15.Multiline = true;
+            textBox15.Name = "textBox15";
+            textBox15.Size = new Size(626, 89);
+            textBox15.TabIndex = 1;
+            // 
+            // textBox16
+            // 
+            textBox16.Location = new Point(172, 551);
+            textBox16.Name = "textBox16";
+            textBox16.Size = new Size(159, 23);
+            textBox16.TabIndex = 1;
+            textBox16.TextChanged += textBox16_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(250, 255, 254);
+            BackgroundImage = Properties.Resources.Captura_de_tela_2026_02_16_162357;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(734, 661);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox4);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox8);
+            Controls.Add(textBox15);
+            Controls.Add(textBox14);
+            Controls.Add(textBox12);
+            Controls.Add(textBox16);
+            Controls.Add(textBox11);
+            Controls.Add(textBox9);
+            Controls.Add(textBox13);
+            Controls.Add(textBox5);
+            Controls.Add(textBox3);
+            Controls.Add(textBox10);
+            Controls.Add(textBox1);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label16);
+            Controls.Add(label12);
+            Controls.Add(label15);
+            Controls.Add(label11);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Cadastro de clientes";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private Label label16;
+        private TextBox textBox11;
+        private TextBox textBox12;
+        private TextBox textBox13;
+        private TextBox textBox14;
+        private TextBox textBox15;
+        private TextBox textBox16;
+    }
+}
---
+++ Cadastro de clientes visual studio/Form1.cs
@@ -0,0 +1,37 @@
+namespace Cadastro_de_clientes_visual_studio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Handler required by the designer (label1.Click += this.label1_Click)
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // intentionally left empty
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox16_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
